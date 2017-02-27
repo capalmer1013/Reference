40bb8fb4b5f75fac0b03e51bd0b777ec30cb9b64
Chris Palmer <capalmer1013@gmail.com>
2017-02-27 14:13:23 -0500

    trying out this cool looking C++ testing suite

---
+++ C++/testing/catch/README.md
@@ -0,0 +1,7 @@
+# Example of catch
+All stolen from [here](https://github.com/philsquared/Catch/blob/master/docs/tutorial.md)
+
+- comples to a complete executable which responds to command line arguments.
+- no arguments, executes all test cases, report failures, and summary of how may tests passed and failed
+Command line reference [link](https://github.com/philsquared/Catch/blob/master/docs/command-line.md)
+
---
+++ C++/testing/catch/example.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN  // this tells catch to provide a main() - only do this in once cpp file
+#include "catch.hpp"
+
+unsigned int Factorial( unsigned int number) {
+	if(number == 0) return 1;
+	return number <= 1 ? number: Factorial(number-1) * number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]") {
+	REQUIRE( Factorial(0) == 1);
+	REQUIRE( Factorial(1) == 1);
+	REQUIRE( Factorial(2) == 2);
+	REQUIRE( Factorial(3) == 6);
+	REQUIRE( Factorial(10) == 3628800);
+}
+
